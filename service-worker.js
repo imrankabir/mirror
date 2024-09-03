69628b28c5c3e120b5d39f155ab6de777c2488a3
Imran Kabir <imran.kabir.k@gmail.com>
2024-09-03 17:25:25 +0500

    mirror auto commit on 2024-09-03 05:25:25 PM

--- service-worker.js
+++ service-worker.js
@@ -1,79 +1,48 @@
-const CACHE_NAME = '1';
-const urlsToCache = [
-  '/',
-  'index.html',
-  'style.css',
-  'script.js',
-  'favicon.png',
-  'icon-192x192.png',
-  'icon-512x512.png',
-  'manifest.json',
-];
+const CACHE_NAME = 'mirror-1';
+const urlsToCache = ['/', 'index.html', 'style.css', 'script.js', 'favicon.png', 'icon-192x192.png', 'icon-512x512.png', 'manifest.json'];
 
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
 
-// self.addEventListener('install', event => {
-//   // Perform install steps
-//   event.waitUntil(
-//     caches.open(CACHE_NAME)
-//       .then(cache => {
-//         console.log('Opened cache');
-//         return cache.addAll(urlsToCache);
-//       })
-//   );
-// });
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
 
-self.addEventListener('activate', event => event.waitUntil(caches.keys().then(cacheNames => Promise.all(cacheNames.map(cacheName => [CACHE_NAME].indexOf(cacheName) === -1); caches.delete(cacheName)))));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response;
+      }
+      
+      const fetchRequest = event.request.clone();
+      return fetch(fetchRequest).then(response => {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
 
-// self.addEventListener('activate', event => {
-//   const cacheWhitelist = [CACHE_NAME];
-//   event.waitUntil(
-//     caches.keys().then(cacheNames => {
-//       return Promise.all(
-//         cacheNames.map(cacheName => {
-//           if (cacheWhitelist.indexOf(cacheName) === -1) {
-//             return caches.delete(cacheName);
-//           }
-//         })
-//       );
-//     })
-//   );
-// });
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseToCache);
+        });
 
-self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(response => response || fetch(event.request.clone()).then(response => (!response || response.status !== 200 || response.type !== 'basic') ? response : caches.open(CACHE_NAME).then(cache => cache.put(event.request, response.clone()))))));
-
-// self.addEventListener('fetch', event => {
-//   event.respondWith(
-//     caches.match(event.request)
-//       .then(response => {
-//         // Cache hit - return response
-//         if (response) {
-//           return response;
-//         }
-
-//         // IMPORTANT: Clone the request. A request is a stream and can only be consumed once.
-//         // Since we are consuming this once by cache and once by the browser for fetch, we need to clone the response.
-//         const fetchRequest = event.request.clone();
-
-//         return fetch(fetchRequest).then(
-//           response => {
-//             // Check if we received a valid response
-//             if (!response || response.status !== 200 || response.type !== 'basic') {
-//               return response;
-//             }
-
-//             // IMPORTANT: Clone the response. A response is a stream and because we want the browser to consume the response
-//             // as well as the cache consuming the response, we need to clone it so we have two streams.
-//             const responseToCache = response.clone();
-
-//             caches.open(CACHE_NAME)
-//               .then(cache => {
-//                 cache.put(event.request, responseToCache);
-//               });
-
-//             return response;
-//           }
-//         );
-//       })
-//   );
-// });
+        return response;
+      });
+    })
+  );
+});
