f50a4e423749970b1cf6af911d8d9f871ff88e52
Imran Kabir <imran.kabir.k@gmail.com>
2024-09-03 13:31:56 +0500

    mirror auto commit on 2024-09-03 01:31:56 PM

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,24 @@
 self.addEventListener('install', event => {
-  console.log('Service Worker installing.');
-  // Cache files or perform other tasks
+  event.waitUntil(
+    caches.open('static-cache-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/favicon.png',
+        '/icon-192x192.png',
+        '/icon-512x512.png',
+        '/manifest.json',
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
